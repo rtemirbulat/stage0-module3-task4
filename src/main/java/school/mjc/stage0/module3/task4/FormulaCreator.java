9df5f3e1e9d1908c2343417348b2927aece292c8
rtemirbulat <equalityisnotequality@gmail.com>
2024-01-24 16:18:11 +0600

    final

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -1,6 +1,10 @@
 package school.mjc.stage0.module3.task4;
 
+
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 2, b = 2;
+        double formula = (9 * Math.pow(a,2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+
     }
 }
